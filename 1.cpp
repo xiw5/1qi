71e06c5af03f10b50e28e7e64ce0bf82e517027a
xiw5 <1404861473@qq.com>
2017-11-02 07:13:14 +0000

    gai

--- 1.cpp
+++ 1.cpp
@@ -31,18 +31,18 @@ void qing()
 //	if(map_it==ind.end())
 //	  fout<<"Y"<<"\n";
   for(;map_it!=ind.end();map_it++)
-	  {
+	{
 //	     printf("2");
-             string ss=map_it->first;
-	     fout<<ss<<" ";
-	     for(int i=0;i!=ind[ss].size();i++)       
+    string ss=map_it->first;
+	  fout<<ss<<" ";
+	  for(int i=0;i!=ind[ss].size();i++)       
 		{
 		   fout<<ind[ss][i].first<<":"<<ind[ss][i].second; 
 		   if(i!=ind[ss].size()-1)
 		     fout<<" ";
 		}
-	     fout<<"\n";
-	  }
+	  fout<<"\n";
+	}
 	ind.clear();
 //	fout<<"dsffs";
   fout.close();
@@ -59,145 +59,151 @@ void chuli(char *cc,int a)
   for(;map_it!=x.end();map_it++)
 	  {
 		  pa pa1;
-                  pa1=make_pair(a,map_it->second);
+      pa1=make_pair(a,map_it->second);
 		 // printf("1");
-                  ind[map_it->first].push_back(pa1);
+      ind[map_it->first].push_back(pa1);
 	  }	  
-        fin.close();  
+  fin.close();  
 	if(ind.size()>2000000)
-        qing();  
+    qing();  
 }
 void dfs(char *cc)
 {
 	struct dirent *wenjian;
 	DIR *dir;
-        dir=opendir(cc);
+    dir=opendir(cc);
 	if(dir!=NULL)
 	  for(;(wenjian=readdir(dir))!=NULL;)
-    	   {
-              char ch3[256]; 
-	      struct stat s;
-	      sprintf(ch3,"%s/%s",cc,wenjian->d_name);			  
+    {
+      char ch3[256]; 
+	    struct stat s;
+	    sprintf(ch3,"%s/%s",cc,wenjian->d_name);			  
 	     // printf("%s\n",wenjian->d_name);
 	     // printf("%d\n",strncmp(wenjian->d_name,".",1));
-	      lstat(ch3,&s);
-	      if(S_ISDIR(s.st_mode))
-		{
-			if(strncmp(wenjian->d_name,".",1))
-                                  dfs(ch3);
-		}
-	      else
-		{   
-			   SUM++;  
-				 CH[SUM]=(char *)malloc(strlen(ch3)+1);
-         for(int i=0;i<strlen(ch3);i++)
-					 CH[SUM][i]=ch3[i];
-				 CH[SUM][strlen(ch3)]='\0';
+	    lstat(ch3,&s);
+	    if(S_ISDIR(s.st_mode))
+		  {
+			  if(strncmp(wenjian->d_name,".",1))
+          dfs(ch3);
+		  }
+	    else
+		  {   
+			  SUM++;  
+				CH[SUM]=(char *)malloc(strlen(ch3)+1);
+        for(int i=0;i<strlen(ch3);i++)
+				  CH[SUM][i]=ch3[i];
+				CH[SUM][strlen(ch3)]='\0';
 //	          printf("%s\n",ch3);
-         chuli(ch3,SUM);
-		}
+        chuli(ch3,SUM);
+	 	  }
 	   }
 	closedir(dir);
 
 }
 void bing(int l,int r)
 {
-   char ch1[100],ch2[100],cc[10],cc1[10],ch11[MX],ch22[MX],ch3[8]="zhong";
-   sprintf(cc,"text%d",l);
-   ifstream fin(cc);
-   sprintf(cc1,"text%d",(l+r)/2+1);
-   ifstream fin1(cc1);
-   fin>>ch1;
-   fin1>>ch2;
-   creat(ch3,0777);
-   fin.getline(ch11,MX);
-   fin1.getline(ch22,MX);
-   ofstream fout(ch3);
-   int kg=0;   
-   for(;;)
-    {
-      if(ch1[0]=='\0')
-	   {
-	     kg=1;
-	     break;
-	   }
-      if(ch2[0]=='\0')
-	   {
-	     kg=2;
-	     break;
-	   }
-      if(strcmp(ch1,ch2)==0)
-        {
-          fout<<ch1<<ch11<<ch22<<"\n";
-          if(!(fin>>ch1))
+  char ch1[100],ch2[100],cc[10],cc1[10],ch11[MX],ch22[MX],ch3[8]="zhong";
+  sprintf(cc,"text%d",l);
+  ifstream fin(cc);
+	sprintf(cc1,"text%d",(l+r)/2+1);
+	ifstream fin1(cc1);
+  fin>>ch1;
+  fin1>>ch2;
+  creat(ch3,0777);
+  fin.getline(ch11,MX);
+  fin1.getline(ch22,MX);
+  ofstream fout(ch3);
+	int kg=0;   
+  for(;;)
+  {
+    if(ch1[0]=='\0')
 	  {
-             kg=1;
-	     break;
+	    kg=1;
+	    break;
 	  }
-	  fin.getline(ch11,MX);
-	  if(!(fin1>>ch2))
+    if(ch2[0]=='\0')
 	  {
-	     kg=2;
-	     break;
+	    kg=2;
+	    break;
 	  }
-	  fin1.getline(ch22,MX);
-	}
-      if(strcmp(ch1,ch2)<0)
-	{
-          fout<<ch1<<ch11<<"\n";
-	  if(!(fin>>ch1))
-          {
-	     kg=1;
-	     break;
+    if(strcmp(ch1,ch2)==0)
+    {
+      fout<<ch1<<ch11<<ch22<<"\n";
+      if(!(fin>>ch1))
+	    {
+        kg=3;
+	      break;
+	    }
+	    fin.getline(ch11,MX);
+	    if(!(fin1>>ch2))
+	    {
+	      kg=2;
+	      break;
+	    }
+	    fin1.getline(ch22,MX);
 	  }
-	  fin.getline(ch11,MX);	
-	}
-      if(strcmp(ch1,ch2)>0)
-	{
-          fout<<ch2<<ch22<<"\n";
-	  if(!(fin1>>ch2))
+    if(strcmp(ch1,ch2)<0)
 	  {
-	     kg=2;
-	     break;
+      fout<<ch1<<ch11<<"\n";
+	    if(!(fin>>ch1))
+      {
+	      kg=1;
+	      break;
+	    }
+	    fin.getline(ch11,MX);	
 	  }
-	  fin1.getline(ch22,MX);	
-	}
-    }
-   if(kg==2)
-   for(;;) 
+    if(strcmp(ch1,ch2)>0)
+	  {
+      fout<<ch2<<ch22<<"\n";
+	    if(!(fin1>>ch2))
+	    {
+	      kg=2;
+	      break;
+	    }
+	    fin1.getline(ch22,MX);	
+	  }
+  }
+  if(kg==2)
+    for(;;) 
     {
-	 fout<<ch1<<ch11<<"\n";
-         if(!(fin>>ch1))
-	   break;
-	 fin.getline(ch11,MX);
+	    fout<<ch1<<ch11<<"\n";
+      if(!(fin>>ch1))
+	      break;
+	    fin.getline(ch11,MX);
     }
-   if(kg==1)
-   for(;;) 
+  if(kg==1)
+    for(;;) 
     {
-	 fout<<ch2<<ch22<<"\n";
-	 if(!(fin1>>ch2))
-	   break;
-	 fin1.getline(ch22,MX);
+	    fout<<ch2<<ch22<<"\n";
+	    if(!(fin1>>ch2))
+	      break;
+	    fin1.getline(ch22,MX);
     }
-   fin.close();
-   fin1.close();
-   remove(cc);
-   remove(cc1);
-   rename(ch3,cc);
+	if(kg==3)
+		for(;fin1>>ch2;)
+		{
+			fin1.getline(ch22,MX);
+			fout<<ch2<<ch22<<"\n";
+		}
+  fin.close();
+  fin1.close();
+  remove(cc);
+  remove(cc1);
+  rename(ch3,cc);
 }
 void guibing(int l,int r)
 {
-   if(l==r)
-     return;
-   if(l==r-1)
-   {
-      bing(l,r);
-      return;
-   }
-   int mid=(l+r)/2;
-   guibing(l,mid);
-   guibing(mid+1,r);
-   bing(l,r);
+  if(l==r)
+    return;
+  if(l==r-1)
+  {
+    bing(l,r);
+    return;
+  }
+  int mid=(l+r)/2;
+  guibing(l,mid);
+  guibing(mid+1,r);
+  bing(l,r);
 }
 int main()
 { 
@@ -215,3 +221,7 @@ int main()
 	return 0;
 }
 //gui bing 1 hang
+//61 
+//18 1000000 wenbenshu
+//105 1000000 yihang
+//105 100 yigedanci
